560e5323e7e5cb0ec65708c36c24dcfc422f1a7b
Patrice Buriez <patrice.buriez@chenapan.org>
2025-05-28 03:31:37 +0200

    rapidPROX: remove old OPNFV/Anuket contents: VNF_Catalogue/

--- VNF_Catalogue/cronjob/script.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-/usr/local/bin/node /usr/src/app/github >> /tmp/mybacklog 2>&1
--- VNF_Catalogue/migration/bash.sh
+++
@@ -1,4 +0,0 @@
-DB_HOST='localhost';
-DB_USER='root';
-DB_PASSWORD='iiit123';
-DB_DATABASE='vnf_catalogue';
